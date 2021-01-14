c15306896a1345d1359f44ec1a37305c314f9a0e
opeyemi Abass <yemigold11@gmail.com>
2021-01-14 14:25:00 -0600

    images can be display now on the home page

--- app/src/main/java/com/example/com/book_app2/MainActivity.java
+++ app/src/main/java/com/example/com/book_app2/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.SearchView;
 
 /**
@@ -15,6 +16,7 @@ import android.widget.SearchView;
 
 public class MainActivity extends AppCompatActivity  {
 
+    ImageView imageView;
 
 
     @Override
@@ -23,14 +25,25 @@ public class MainActivity extends AppCompatActivity  {
         setContentView(R.layout.main_activity_layout);
 
 
+        imageView = findViewById(R.id.homeImage);
         SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
         final SearchView searchView = (SearchView) findViewById(R.id.mainsearch2);
         searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
 
-
         searchView.setIconifiedByDefault(false); // Do not iconify the widget; expand it by default
 
+        getImage();
 
+    }
+
+
+    private void getImage(){
+        //using glide to load the image
+        GlideApp.with(getApplicationContext())
+                .load("https://photostream.iastate.edu/public/002/2511/2511-large-10fc1e05585c768e.jpg")
+                .fitCenter()
+                .centerCrop()
+                .into(imageView);
     }
 
 
